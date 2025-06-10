d85c553bc725b9a245129b095c492793d9e173be
JustinKt25 <justinpros25@gmail.com>
2025-06-11 01:27:25 +0700

    Save Script

--- Assets/Script/GameOver.cs
+++ Assets/Script/GameOver.cs
@@ -7,7 +7,7 @@ public class GameOver : MonoBehaviour
 {
     public GameObject gameOverPanel;
 
-    // Update is called once per frame
+    
     void Update()
     {
         if (GameObject.FindGameObjectWithTag("Player") == null)
